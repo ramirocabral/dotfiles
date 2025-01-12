6ffc70f17f27810e6825f6b70edb0c7f684cd62b
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2025-01-12 20:59:31 -0300

    idk

--- .config/nvim/lua/ramiro/plugins/lsp/mason.lua
+++ .config/nvim/lua/ramiro/plugins/lsp/mason.lua
@@ -1,7 +1,6 @@
 -- import mason plugin safely
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-	print("asdf")
 	return
 end
 
@@ -37,7 +36,7 @@ mason_lspconfig.setup({
 		"clangd",
 		"texlab",
 		"pyright",
-        "gopls",
+		"gopls",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
