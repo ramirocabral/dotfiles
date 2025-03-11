615c8db599d94076707bd10f25342898428d69f4
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2025-03-11 17:38:23 -0300

    tailwindcss colors autocompletion

--- .config/nvim/lua/ramiro/plugins-setup.lua
+++ .config/nvim/lua/ramiro/plugins-setup.lua
@@ -64,10 +64,11 @@ return packer.startup(function(use)
 	use("hrsh7th/nvim-cmp") -- completion plugin
 	use("hrsh7th/cmp-buffer") -- source for text in buffer
 	use("hrsh7th/cmp-path") -- source for file system paths
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
 
 	-- snippets
 	use("L3MON4D3/LuaSnip") -- snippet engine
-	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
 	use("rafamadriz/friendly-snippets") -- useful snippets
 
 	-- managing & installing lsp servers, linters & formatters
@@ -76,10 +77,8 @@ return packer.startup(function(use)
 
 	-- configuring lsp servers
 	use("neovim/nvim-lspconfig") -- easily configure language servers
-	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
 
 	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
 	-- formatting & linting
 	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
@@ -106,15 +105,6 @@ return packer.startup(function(use)
 
 	--copilot
 	use("github/copilot.vim")
-
-	-- floating terminal
-	use({
-		"akinsho/toggleterm.nvim",
-		tag = "*",
-		config = function()
-			require("toggleterm").setup()
-		end,
-	})
 
 	use ({
 		"ThePrimeagen/harpoon",
