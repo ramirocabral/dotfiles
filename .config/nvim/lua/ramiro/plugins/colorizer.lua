7843330519e0629e32a26181a09803887653098e
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2025-12-14 00:27:57 -0300

    updated nvim config

--- .config/nvim/lua/ramiro/plugins/colorizer.lua
+++ .config/nvim/lua/ramiro/plugins/colorizer.lua
@@ -6,7 +6,8 @@ end
 
 -- configure colorizer
 colorizer.setup({
-	"*",
+	"html",
+	"css",
 	html = { names = false }, -- Disable all nameless colors in html and css.
 	css = { rgb_fn = true, names = false }, -- Enable parsing rgb(...) functions in css and disable all named colors.
 })
